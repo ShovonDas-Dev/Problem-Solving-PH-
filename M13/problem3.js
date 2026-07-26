b8428b040f9c8114a4e18431061cc731c72e3137
ShovonDas-Dev <shovon.das@genexinfosys.com>
2026-07-26 12:39:40 +0600

    problem 3 solved

---
+++ M13/problem3.js
@@ -0,0 +1,15 @@
+// Task 13-3A: Product of Digits
+// Problem Statement:
+// Write a function productOfDigits that takes a positive whole number and returns the product of its individual digits.
+
+    function productDigits (product) {
+        let ans = 1
+        let number = product.toString().split("")
+        for (let num= 0; num < number.length; num++){
+             ans =  ans * number[num]
+        }
+        return ans
+    }
+
+    console.log(productDigits(258))
+    
